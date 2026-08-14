c553f298b5690aec4f347b23f9d9e4773834a5c6
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-15 00:46:59 +0530

    feat: add leave, kick, and delete server routes

--- src/routes/servers.js
+++ src/routes/servers.js
@@ -1,4 +1,4 @@
-﻿const express = require('express');
+const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
 const {
@@ -7,6 +7,9 @@ const {
   listUserServersController,
   createChannelController,
   listChannelsController,
+  leaveServerController,
+  kickMemberController,
+  deleteServerController,
 } = require('../controllers/serverController');
 
 router.use(authMiddleware);
@@ -16,5 +19,8 @@ router.get('/', listUserServersController);
 router.post('/join/:inviteCode', joinServerController);
 router.post('/:serverId/channels', createChannelController);
 router.get('/:serverId/channels', listChannelsController);
+router.delete('/:serverId/leave', leaveServerController);
+router.delete('/:serverId/members/:userId', kickMemberController);
+router.delete('/:serverId', deleteServerController);
 
 module.exports = router;
