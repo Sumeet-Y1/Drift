f1e1136a059b4a44e549891941ce9c2a0fbd8901
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-15 00:37:05 +0530

    feat: add channels route for message history

---
+++ src/routes/channels.js
@@ -0,0 +1,10 @@
+﻿const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middleware/auth');
+const { getMessagesController } = require('../controllers/messageController');
+
+router.use(authMiddleware);
+
+router.get('/:channelId/messages', getMessagesController);
+
+module.exports = router;
