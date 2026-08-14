d4845863f4d383a6a4fb5f4831bc96ee6400324d
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-15 00:37:05 +0530

    feat: add message controller

---
+++ src/controllers/messageController.js
@@ -0,0 +1,8 @@
+﻿const messageService = require('../services/messageService');
+
+async function getMessagesController(req, res) {
+  const messages = await messageService.getMessages(req.user.userId, req.params.channelId);
+  res.status(200).json(messages);
+}
+
+module.exports = { getMessagesController };
