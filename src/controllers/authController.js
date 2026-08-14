758aa82e301ee53bb9536a46b3fcbb4837de9e75
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-15 00:28:23 +0530

    feat: add auth controller

---
+++ src/controllers/authController.js
@@ -0,0 +1,16 @@
+﻿const authService = require('../services/authService');
+const { signupSchema, loginSchema } = require('../validators/authValidator');
+
+async function signupController(req, res) {
+  const parsed = signupSchema.parse(req.body);
+  const result = await authService.signup(parsed.username, parsed.email, parsed.password);
+  res.status(201).json(result);
+}
+
+async function loginController(req, res) {
+  const parsed = loginSchema.parse(req.body);
+  const result = await authService.login(parsed.email, parsed.password);
+  res.status(200).json(result);
+}
+
+module.exports = { signupController, loginController };
