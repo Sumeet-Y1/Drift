a24491cbdd71bd71fbc0605e1fecdd90fa880f7f
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-15 00:31:55 +0530

    feat: mount server routes in express app

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.get('/health', (req, res) => {
 
 // --- Routes (mounted as we build them) ---
 app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/servers', require('./routes/servers'));
+app.use('/api/servers', require('./routes/servers'));
 // app.use('/api/channels', require('./routes/channels'));
 
 // --- Error handler (must be last) ---
