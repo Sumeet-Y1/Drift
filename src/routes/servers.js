219f8cd4779bcdfd6f73fb306e480bb436715083
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-15 00:31:55 +0530

    feat: add server and channel routes

---
+++ src/routes/servers.js
@@ -0,0 +1,20 @@
+﻿const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middleware/auth');
+const {
+  createServerController,
+  joinServerController,
+  listUserServersController,
+  createChannelController,
+  listChannelsController,
+} = require('../controllers/serverController');
+
+router.use(authMiddleware);
+
+router.post('/', createServerController);
+router.get('/', listUserServersController);
+router.post('/join/:inviteCode', joinServerController);
+router.post('/:serverId/channels', createChannelController);
+router.get('/:serverId/channels', listChannelsController);
+
+module.exports = router;
