608e1f3563a7cef956a3ae3642e360f78a97a72c
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-15 00:23:46 +0530

    feat: add server entrypoint with http and socket.io setup

---
+++ server.js
@@ -0,0 +1,26 @@
+﻿const http = require('http');
+const { Server } = require('socket.io');
+const app = require('./src/app');
+
+const PORT = process.env.PORT || 4000;
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('Socket connected: ' + socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('Socket disconnected: ' + socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log('Drift server running on http://localhost:' + PORT);
+});
