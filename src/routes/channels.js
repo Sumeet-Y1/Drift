2a00b69f4243afd63bf4e325026b3bd530564b62
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-15 00:44:02 +0530

    feat: add edit and delete message routes

--- src/routes/channels.js
+++ src/routes/channels.js
@@ -1,10 +1,16 @@
-﻿const express = require('express');
+const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
-const { getMessagesController } = require('../controllers/messageController');
+const {
+  getMessagesController,
+  editMessageController,
+  deleteMessageController,
+} = require('../controllers/messageController');
 
 router.use(authMiddleware);
 
 router.get('/:channelId/messages', getMessagesController);
+router.patch('/:channelId/messages/:messageId', editMessageController);
+router.delete('/:channelId/messages/:messageId', deleteMessageController);
 
 module.exports = router;
