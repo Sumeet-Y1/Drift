089a256128dede8ecbb164daf3da1270d8998e61
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-15 00:44:02 +0530

    feat: add edit and delete message controllers

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,8 +1,19 @@
-﻿const messageService = require('../services/messageService');
+const messageService = require('../services/messageService');
 
 async function getMessagesController(req, res) {
   const messages = await messageService.getMessages(req.user.userId, req.params.channelId);
   res.status(200).json(messages);
 }
 
-module.exports = { getMessagesController };
+async function editMessageController(req, res) {
+  const { content } = req.body;
+  const message = await messageService.editMessage(req.user.userId, req.params.messageId, content);
+  res.status(200).json(message);
+}
+
+async function deleteMessageController(req, res) {
+  const message = await messageService.deleteMessage(req.user.userId, req.params.messageId);
+  res.status(200).json(message);
+}
+
+module.exports = { getMessagesController, editMessageController, deleteMessageController };
