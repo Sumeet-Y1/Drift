488b2a81d184f342924daa4782ea21522fe09f57
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-15 23:38:18 +0530

    feat: add mute and disconnect routes

--- src/routes/voice.js
+++ src/routes/voice.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
-const { getVoiceTokenController } = require('../controllers/voiceController');
+const {
+  getVoiceTokenController,
+  muteParticipantController,
+  disconnectParticipantController,
+} = require('../controllers/voiceController');
 
 router.use(authMiddleware);
 
 router.get('/:channelId/token', getVoiceTokenController);
+router.post('/:channelId/mute/:userId', muteParticipantController);
+router.post('/:channelId/disconnect/:userId', disconnectParticipantController);
 
 module.exports = router;
