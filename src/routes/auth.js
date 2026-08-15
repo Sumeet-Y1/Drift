83fb184f9dbed367e57fe43cca0d0936c98d69ea
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-15 19:59:38 +0530

    feat: add refresh and logout routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,15 @@
-﻿const express = require('express');
+const express = require('express');
 const router = express.Router();
-const { signupController, loginController } = require('../controllers/authController');
+const {
+  signupController,
+  loginController,
+  refreshController,
+  logoutController,
+} = require('../controllers/authController');
 
 router.post('/signup', signupController);
 router.post('/login', loginController);
+router.post('/refresh', refreshController);
+router.post('/logout', logoutController);
 
 module.exports = router;
